1b225ab8619a41283b48ca2591504e04af68889b
Elton0000 <nhanelton@gmail.com>
2025-04-09 06:59:37 -0700

    added javascript to = button

--- assignments/assignment-4/hello.js
+++ assignments/assignment-4/hello.js
@@ -1,7 +1,8 @@
 const greeting = "JavaScript";
+let x = 0;
 
-const createMessage = (name) => {
-    return `Hello, ${name}!`;
+const createMessage = (x) => {
+    return `${x + 3}`;
 };
 
-console.log(createMessage(greeting));
+console.log(createMessage(0));
